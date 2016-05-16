243aad8f47c0e57af44b7e44fe27f03e7a6f174b
joetheory <joetheory@gmail.com>
2016-05-15 20:51:00 -0400

    Remove dev keys

--- index.js
+++ index.js
@@ -3,45 +3,31 @@ var moment 		= require('moment');
 var http 		= require('http');
 
 var apiUrlBase 	= 'http://api.smitegame.com/smiteapi.svc/';
-var requestType = 'json';
-var timestamp	=	null;
-var session 	=	null;
+var Config = require("./config.json");
 
-//Establish config
-var config = {
-    devId: '',
-    authKey: ''
-};
+var session = generateSession();
 
+function generateSession() {
 
-session = genSession();
-console.log(session.session_id);
-
-function genSession(options){
-
-    var devId = config.devId;
-    var authKey = config.authKey;
+	var devId = Config.devId;
+    var authKey = Config.authKey;
     var utcTime = new moment().utc().format("YYYYMMDDHHmmss");
     var authHash = md5(devId + "createsession" + authKey + utcTime);
     var baseUrl = 'http://api.smitegame.com/smiteapi.svc/createsessionjson/';
     var sessionUrl = baseUrl + devId + '/' + authHash + '/' + utcTime;
 
+    var test = http.get(sessionUrl, function(response) {
+        // Continuously update stream with data
+        var body = '';
+        response.on('data', function(d) {
+            body += d;
+        });
+        response.on('end', function() {
+            // Data reception is done, do whatever with it!
+            var sessionObj = JSON.parse(body);
+            console.log(sessionObj);
+	    });
+	});
 
-   	callback = function(response) {
-		var str = '';
-
-		//another chunk of data has been recieved, so append it to `str`
-		response.on('data', function (chunk) {
-			str += chunk;
-		});
-
-		//the whole response has been recieved, so we just print it out here
-		response.on('end', function () {
-			data = JSON.parse(str);
-			return data;
-		});
-	}
-
-	http.request(sessionUrl, callback).end();
-
- }
+	return test;
+}
